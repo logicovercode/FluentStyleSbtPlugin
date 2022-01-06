ef1847e45ce5c93da11c0ab7b411ae03c8bed3c9
techLeadAtLogicOverCode <techLead@logicovercode.com>
2022-01-06 12:17:46 +0530

    Resolver and License providers removed

--- plugin/com/logicovercode/base_plugin/FluentStyleSbt.scala
+++ plugin/com/logicovercode/base_plugin/FluentStyleSbt.scala
@@ -2,8 +2,6 @@ package com.logicovercode.base_plugin
 
 import com.logicovercode.bsbt.BuilderStyleBuild
 import com.logicovercode.fsbt.commons.dependencies.AllDependencies
-import com.logicovercode.fsbt.commons.licenses.LicenseProvider
-import com.logicovercode.fsbt.commons.resolvers.ResolverProvider
 import com.logicovercode.fsbt.commons.services.{ClusterServicesProvider, MySqlServiceProvider, PostgresSqlServiceProvider}
 import sbt.{AutoPlugin, PluginTrigger, Plugins}
 
@@ -14,8 +12,6 @@ object FluentStyleSbt extends AutoPlugin {
       with MySqlServiceProvider
       with PostgresSqlServiceProvider
       with ClusterServicesProvider
-      with LicenseProvider
-      with ResolverProvider
       //with ProtoSettings
       with FSbtTypeAliases
 
